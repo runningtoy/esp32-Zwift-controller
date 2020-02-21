eda4dbc41943faba295fd2c88e70e622702409a9
gitlab@runningtoy.de <gitlab@runningtoy.de>
2020-02-21 23:26:49 +0100

    LIB Update

--- src/conf.h
+++ src/conf.h
@@ -1,8 +1,5 @@
-#pragma once
 
-#include <BLEHIDDevice.h>
-
-#define DEVICE_NAME "zwift_Shortcut"
+#define DEVICE_NAME "zwiftShortcut"
 
 #define RIGHT_TURN_PIN 25
 #define LEFT_TURN_PIN 26
@@ -13,85 +10,5 @@
 // Sleep after 5 minutes
 #define MAX_ACTIVE_TIME 5 * 60 * 1000
 
-/*
- * HOME  - 0x4a
- * END   - 0x4d
- * RIGHT - 0x4f
- * LEFT  - 0x50
- * DOWN  - 0x51
- * UP    - 0x52
- */
-#define KEY_CODE_LEFT_ARROW 0x50
-#define KEY_CODE_RIGHT_ARROW 0x4f
-#define KEY_CODE_PAGE_UP 0x4B
-#define KEY_CODE_PAGEDOWN 0x4E
-#define KEY_CODE_F10 0x43
 
 //https://zwiftinsider.com/keyboard-shortcuts/
-
-const uint8_t KEYBOARD_REPORT_MAP[] PROGMEM = {
-    USAGE_PAGE(1),
-    0x01, // Generic Desktop Ctrls
-    USAGE(1),
-    0x06, // Keyboard
-    COLLECTION(1),
-    0x01, // Application
-    REPORT_ID(1),
-    0x01, // REPORTID
-    USAGE_PAGE(1),
-    0x07, //   Kbrd/Keypad
-    USAGE_MINIMUM(1),
-    0xE0,
-    USAGE_MAXIMUM(1),
-    0xE7,
-    LOGICAL_MINIMUM(1),
-    0x00,
-    LOGICAL_MAXIMUM(1),
-    0x01,
-    REPORT_SIZE(1),
-    0x01, //   1 byte (Modifier)
-    REPORT_COUNT(1),
-    0x08,
-    HIDINPUT(1),
-    0x02, //   Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position
-    REPORT_COUNT(1),
-    0x01, //   1 byte (Reserved)
-    REPORT_SIZE(1),
-    0x08,
-    HIDINPUT(1),
-    0x01, //   Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position
-    REPORT_COUNT(1),
-    0x05, //   5 bits (Num lock, Caps lock, Scroll lock, Compose, Kana)
-    REPORT_SIZE(1),
-    0x01,
-    USAGE_PAGE(1),
-    0x08, //   LEDs
-    USAGE_MINIMUM(1),
-    0x01, //   Num Lock
-    USAGE_MAXIMUM(1),
-    0x05, //   Kana
-    HIDOUTPUT(1),
-    0x02, //   Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile
-    REPORT_COUNT(1),
-    0x01, //   3 bits (Padding)
-    REPORT_SIZE(1),
-    0x03,
-    HIDOUTPUT(1),
-    0x01, //   Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile
-    REPORT_COUNT(1),
-    0x06, //   6 bytes (Keys)
-    REPORT_SIZE(1),
-    0x08,
-    LOGICAL_MINIMUM(1),
-    0x00,
-    LOGICAL_MAXIMUM(1),
-    0x65, //   101 keys
-    USAGE_PAGE(1),
-    0x07, //   Kbrd/Keypad
-    USAGE_MINIMUM(1),
-    0x00,
-    USAGE_MAXIMUM(1),
-    0x65,
-    HIDINPUT(1),
-    0x00, //   Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position
-    END_COLLECTION(0)};
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
 #include <Arduino.h>
 
-#include <BLEDevice.h>
-#include <soc/rtc_cntl_reg.h>
-#include <soc/soc.h>
-
 #include "conf.h"
 #include "log.h"
 
-#include "BLEKeyboard.h"
+#include <BleKeyboard.h>
+
 #include "Button.h"
+
+BleKeyboard bleKeyboard(DEVICE_NAME,"Espressif",100);
 
 Button leftTurn(LEFT_TURN_PIN);
 Button rightTurn(RIGHT_TURN_PIN);
 Button FTPup(FTP_UP_PIN);
 Button FTPdown(FTP_DOWN_PIN);
-Button Screenshoot(SCREENSHOOT_PIN);
+Button Screenshot(SCREENSHOOT_PIN);
+
+
 
 unsigned long lastActiveTime = 0;
 
@@ -27,7 +28,7 @@ void updateButtons() {
   rightTurn.update();
   FTPup.update();
   FTPdown.update();
-  Screenshoot.update();
+  Screenshot.update();
   }
 
 void deepSleep() {
@@ -35,67 +36,52 @@ void deepSleep() {
 }
 
 void setup() {
-  // Disable brownout detector
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
-
   Serial.begin(115200);
   Serial.println();
 
-  LOG_I("zwift_Shortcut - An ESP32 based BLE Keynote remote controller.");
-  LOG_I("based on a design by Henry Li <henry1943@163.com>.");
-  LOG_I("Copyright %d. All rights reserved.", 2018);
+  LOG_I("Initializing BLE HID services...");
+  bleKeyboard.begin();
+  LOG_I("Initialization has been <DONE>.");
 
   // Initialize buttons and laser emitter.
   leftTurn.onPress([]() {
     LOG_D("KEY_CODE_LEFT_ARROW");
-    BLEKeyboard.strokeKey(KEY_CODE_LEFT_ARROW);
+    bleKeyboard.write(KEY_LEFT_ARROW);
     activate();
   });
   leftTurn.begin();
 
   rightTurn.onPress([]() {
     LOG_D("KEY_CODE_RIGHT_ARROW");
-    BLEKeyboard.strokeKey(KEY_CODE_RIGHT_ARROW);
+    bleKeyboard.write(KEY_RIGHT_ARROW);
     activate();
   });
   rightTurn.begin();
 
   FTPup.onPress([]() {
     LOG_D("KEY_CODE_PAGE_UP");
-    BLEKeyboard.strokeKey(KEY_CODE_PAGE_UP);
+    bleKeyboard.write(KEY_PAGE_UP);
     activate();
   });
   FTPup.begin();
 
   FTPdown.onPress([]() {
     LOG_D("KEY_CODE_PAGEDOWN");
-    BLEKeyboard.strokeKey(KEY_CODE_PAGEDOWN);
+    bleKeyboard.write(KEY_PAGE_DOWN);
     activate();
   });
   FTPdown.begin();
 
-  Screenshoot.onPress([]() {
+  Screenshot.onPress([]() {
     LOG_D("KEY_CODE_F10");
-    BLEKeyboard.strokeKey(KEY_CODE_F10);
+    bleKeyboard.write(KEY_F10);
     activate();
   });
-  Screenshoot.begin();
-
-
-  LOG_I("Initializing BLE Device '%s'...", DEVICE_NAME);
-  BLEDevice::init(DEVICE_NAME);
-  LOG_I("Initializing BLE HID services...");
-  BLEKeyboard.begin();
-
-  BLEKeyboard.startAdvertising();
-
-  LOG_I("Initialization has been <DONE>.");
+  Screenshot.begin();
 }
 
 void loop() {
-  updateButtons();
-
-  // if (millis() - lastActiveTime > MAX_ACTIVE_TIME) {
-  //   deepSleep();
-  // }
+  if(bleKeyboard.isConnected()) {
+    updateButtons();
+  }
 }
